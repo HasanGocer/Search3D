e5d60dd20d2c23efbd55f69d3c15000bb6eb1463
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-02-04 20:19:33 +0300

    v 0.37

--- Assets/Scripts/BossTouch.cs
+++ Assets/Scripts/BossTouch.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BossTouch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnMouseDown()
     {
-        
+        HitSystem.Instance.BallJump(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Sorter"))
+        {
+            transform.DOShakeScale(0.5f, 0.7f);
+            Vibration.Vibrate(30);
+            SoundSystem.Instance.CallObjectTouch();
+            HitSystem.Instance.BackObject(other.gameObject);
+            int monetCount = Random.Range(5, 10);
+            StartCoroutine(PointText.Instance.CallPointMoneyText(gameObject, monetCount, true));
+            GameManager.Instance.addedMoney += monetCount;
+        }
+        if (other.CompareTag("Finish"))
+        {
+            gameObject.SetActive(false);
+            ContractSystem.Instance.ContractFinish();
+        }
     }
+
 }
